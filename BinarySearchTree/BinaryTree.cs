e45888c872a4e789598c5bc62d975c56b0feed5c
Zac Melton <zac.melton@outlook.com>
2020-01-30 13:28:15 -0600

    init

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinaryTree
+    {
+        public Node root;
+        public Node current;
+
+        public BinaryTree()
+        {
+
+        }
+
+        public BinaryTree(Node root)
+        {
+            this.root = root;
+        }
+
+        public void Add(int data)
+        {
+
+        }
+    }
+}
